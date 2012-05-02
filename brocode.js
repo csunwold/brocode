9f428c1997c8b15b381ee013949598b92e5850c1
Corey Sunwold <csunwold@gmail.com>
2012-05-01 17:06:33 -0700

    pipe through results for compiling and parsing

--- brocode.js
+++ brocode.js
@@ -1,5 +1,26 @@
-var parser = require("./parser");
-var compiler = require("./jsCompiler");
+var parser = require("./parser.js");
+var compiler = require("./jsCompiler.js");
 
 var arguments = process.argv.splice(2);
-console.log(arguments);
+
+if (arguments.length < 1) {
+	console.log("Bro, nothing to rage on.");
+}
+
+var parseTree = parser.parse(arguments[0]);
+var compiledCode = compiler.compile(parseTree);
+
+var fs = require('fs');
+var fileName = "output.js";
+
+if (arguments.length > 1) {
+	fileName = arguments[1];
+}
+
+fs.writeFile(fileName, compiledCode, function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("Done!");
+    }
+}); 
